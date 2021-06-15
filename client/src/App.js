2b4489431d1969b67bbf787cf757b884d05bce0b
Daniel Sungwon Lee <danlee333@gmail.com>
2021-06-15 16:47:04 -0700

    added router

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './pages/home';
 
 function App() {
   return (
     <div className="App">
+      <Router>
 
+        <Switch>
+
+          <Route exact path="/">
+            <Home />
+          </Route>
+
+        </Switch>
+
+      </Router>
     </div>
   );
 }
