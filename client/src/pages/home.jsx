c862c28cf51f0e70765e33db12cad0c2806fbd62
Daniel Sungwon Lee <danlee333@gmail.com>
2021-06-18 17:45:17 -0700

    loading

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Loading from '../components/loading';
 
 export default function Home (props) {
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    setLoading(false)
+  }, [])
+
   return (
     <div className="container">
       <h1>This or That</h1>
 
-      <Loading />
+      <div>
+        {
+          loading ? <Loading />
+                  : <div>
+
+                    </div>
+        }
+      </div>
+
     </div>
   )
 }
