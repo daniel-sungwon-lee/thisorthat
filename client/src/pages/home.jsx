681a9a0adb24bdcb9909584cca76f5aca5698473
Daniel Sungwon Lee <danlee333@gmail.com>
2021-06-18 18:08:59 -0700

    added cards

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,7 +1,22 @@
 import React, { useEffect, useState } from 'react';
+import { Card, CardContent } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 import Loading from '../components/loading';
 
+const useStyles = makeStyles({
+  card: {
+    borderRadius: "1rem",
+    minHeight: "50vh",
+    width: "100%"
+  },
+  divider: {
+    padding: "0.5rem"
+  }
+})
+
+
 export default function Home (props) {
+  const classes = useStyles();
   const [loading, setLoading] = useState(true)
 
   useEffect(() => {
@@ -15,7 +30,21 @@ export default function Home (props) {
       <div>
         {
           loading ? <Loading />
-                  : <div>
+                  : <div className="d-flex justify-content-between mt-5">
+
+                      <Card raised className={classes.card}>
+                        <CardContent>
+
+                        </CardContent>
+                      </Card>
+
+                      <div className={classes.divider}></div>
+
+                      <Card raised className={classes.card}>
+                        <CardContent>
+
+                        </CardContent>
+                      </Card>
 
                     </div>
         }
