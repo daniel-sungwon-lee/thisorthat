5a09f24d81219684fe8d649c3a78989bfd1dd2fb
Daniel Sungwon Lee <danlee333@gmail.com>
2021-06-18 18:21:51 -0700

    added fab

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import { Card, CardContent } from '@material-ui/core';
+import { Card, CardContent, Fab } from '@material-ui/core';
+import { AddRounded } from '@material-ui/icons';
 import { makeStyles } from '@material-ui/core/styles';
 import Loading from '../components/loading';
 
@@ -7,10 +8,17 @@ const useStyles = makeStyles({
   card: {
     borderRadius: "1rem",
     minHeight: "50vh",
-    width: "100%"
+    width: "100%",
+    position: "relative"
   },
   divider: {
     padding: "0.5rem"
+  },
+  fab: {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)"
   }
 })
 
@@ -35,6 +43,10 @@ export default function Home (props) {
                       <Card raised className={classes.card} style={{background: "#FF9478"}}>
                         <CardContent>
 
+                          <Fab className={classes.fab} style={{background: "#FFEB99"}}>
+                            <AddRounded fontSize="large" style={{color: "#FF9478"}} />
+                          </Fab>
+
                         </CardContent>
                       </Card>
 
@@ -42,6 +54,10 @@ export default function Home (props) {
 
                       <Card raised className={classes.card} style={{background: "#FFEB99"}}>
                         <CardContent>
+
+                          <Fab className={classes.fab} style={{background: "#FF9478"}}>
+                            <AddRounded fontSize="large" style={{color: "#FFEB99"}} />
+                          </Fab>
 
                         </CardContent>
                       </Card>
